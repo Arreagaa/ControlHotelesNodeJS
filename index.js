0c82a2d531c808cefa4e8147fb1e6612c1a5bd1a
Christian Rivas <crivas-2020025@kinal.edu.gt>
2022-07-13 01:01:47 -0600

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ mongoose.Promise = global.Promise;
 mongoose.connect(process.env.CONTROL_HOTELES, { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
     console.log("Se encuentra conectado a la base de datos.");
 
-    app.listen(process.env.PORT || 3000, function () {
+    app.listen(process.env.PORT || 8080, function () {
         console.log('Corriendo en el puerto 3000')
     })
     
