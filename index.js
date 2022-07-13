dc012edbe5483bff9af4a62ae0cb044bf2afb5a9
Gerardo Andree Pérez Cabrera <88795594+gperez2018345@users.noreply.github.com>
2022-07-13 12:53:48 -0600

    j

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ console.log(process.env);
 //const PORT = process.env.PORT || 3000;
 
 mongoose.Promise = global.Promise;                                                                
-mongoose.connect('mongodb+srv://grupo7:arreaga1@grupo7.b3ep0uj.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
+mongoose.connect(process.env.CONTROL_HOTELES, { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
     console.log("Se encuentra conectado a la base de datos.");
 
-    app.listen(5000|| 3000, function () {
+    app.listen(process.env.PORT|| 3000, function () {
         console.log('Corriendo en el puerto 3000')
     })
     
