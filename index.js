4288faa1f5412148269bcf15b9eac138e34f3301
Christian Rivas <crivas-2020025@kinal.edu.gt>
2022-07-13 12:06:30 -0600

    make it better

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb+srv://grupo7:arreaga1@grupo7.b3ep0uj.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
     console.log("Se encuentra conectado a la base de datos.");
 
-    app.listen(8080|| 3000, function () {
+    app.listen(5000|| 3000, function () {
         console.log('Corriendo en el puerto 3000')
     })
     
